9af3aa733f8161378b3187f4c430be5006ab5f8f
UGONT <HU.AHUMADA@DUOCUC.CL>
2023-05-29 18:24:43 -0400

    Cod Python Listas
    codigo listas python

---
+++ example.py
@@ -0,0 +1,11 @@
+lista = [1,5,25,100,500]
+print("Lista 1",lista)
+
+lista.append(250)
+print("Lista 2 Append",lista)
+
+lista.remove(1)
+print("Lista 3 Remove",lista)
+
+lista.pop(2)
+print("Lista 4 Pop",lista)
---
+++ ola.py
@@ -0,0 +1,13 @@
+s = True
+while s == True:
+    try:
+        numero = input("Ingrese un numero: ")
+        lista = []
+
+        for i in numero:
+            lista.append(i)
+        num = int(lista[2]+lista[1]+lista[0])
+        print(num)
+        s = False
+    except:
+        print("Ingresar un NUMERO de 3 DIGITOS")
