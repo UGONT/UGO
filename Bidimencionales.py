d758cc7c74b4ec729e9af2836ad67876cf029f8f
UGONT <134973209+UGONT@users.noreply.github.com>
2023-06-05 18:44:28 -0400

    Create Bidimencionales.py

---
+++ Bidimencionales.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+lista = [[1,2,3],[4,5,6],[7,8,9]]
+arreglo = np.array(lista)
+arr = np.arange(1,101).reshape(10,10)
+print(arr)
+
+numero = int(input("Ingrese numero: "))
+for y in range(10):
+    for x in range(10):
+        if arr[y][x] == numero:
+            arr[y][x] = 0
+print(arr)
+
+print(arr[3:6:,0:5:])
