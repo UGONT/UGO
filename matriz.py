53fa44cf384dbf6d8f3da015a4bc0cafbceac02a
UGONT <134973209+UGONT@users.noreply.github.com>
2023-06-13 17:55:19 -0400

    Update matriz.py

--- matriz.py
+++ matriz.py
@@ -1,30 +1,37 @@
 import numpy as np
 
 lista = []
-lista2 = []
+lista2 = []#Numeros dias
 NumFaenas = int(input("Ingrese numero de faenas: "))
 NumDias = int(input("Ingrese numero de dias: "))
 
+
+
+#matriz plan de produccion
 for i in range(NumFaenas*NumDias):
     lista.append(0)
-
+#columna enumerada 
 for i in range(1,NumDias+1):
     lista2.append(i)
 
+#agregar dato en matriz lista
+for i in range(NumFaenas*NumDias):
+    print(f"Ingrese Tonelada N°{i+1}: ")
+    tonelada= int(input())
+    lista[i] = tonelada
+
+#listas arreglos
 matr = np.array(lista2)
-
-
 
 matriz = np.array(lista)
 matriz2 = matriz.reshape(NumFaenas,NumDias)
-#print(matriz2)
 
-print(matr)
+#matriz enumerada
 for fila in matr:
     print(fila, end=" ")
+print()
 
-
-
+#matriz plan de produccion
 for fila in matriz2:
     for elemento in fila:
         print(elemento, end=" ")
