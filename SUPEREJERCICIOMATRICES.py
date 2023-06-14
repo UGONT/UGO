104c48345d03929a2fc4258b0e0e2aea27108977
UGONT <134973209+UGONT@users.noreply.github.com>
2023-06-13 23:44:59 -0400

    Create SUPEREJERCICIOMATRICES.py

---
+++ SUPEREJERCICIOMATRICES.py
@@ -0,0 +1,56 @@
+import numpy as np
+op = 0
+
+listaFaenas = []
+listaFaenas.append(0)
+print(listaFaenas)
+
+
+
+
+
+
+
+
+
+
+
+while op != 5:
+    print("1.Generar plan de produccion")
+    print("2.Ingresar datos de produccion")
+    print("3.Calcular produccion total de una faena especifica")
+    print("4.Calcular produccion total de todas las faenas en un dia especifico")    
+    print("5.Salir del programa")
+    op = input("Ingresa una opcion")
+    if op == 1:
+        print("---Generar plan de produccion---")
+        NumFaenas = int(input("Ingrese numero de faenas: "))
+        NumDias = int(input("Ingrese numero de dias de produccion: "))
+            
+
+
+
+    elif op == 2:
+        print("---Ingresar datos de produccion---")
+
+
+
+
+
+    elif op == 3:
+        print("---Calcular produccion total de una faena especifica---")
+
+
+
+
+
+    elif op == 4:
+        print("---Calcular produccion total de todas las faenas en un dia especifico---")
+
+
+
+
+
+    elif op == 5:
+        print("---Salir del programa---")
+        op = 5
