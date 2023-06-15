0caa086edc2cd3ebe61d3a591e67c097c658f7bc
UGONT <134973209+UGONT@users.noreply.github.com>
2023-06-15 01:43:13 -0400

    Update SUPEREJERCICIOMATRICES copy.py

--- SUPEREJERCICIOMATRICES copy.py
+++ SUPEREJERCICIOMATRICES copy.py
@@ -13,6 +13,15 @@ def Enumeracion_dias():#columna enumerada
         lista2.append(i)
     return lista2    
 
+def arreglo_listas(lista1,lista2):
+    arreglo_lista2 = np.array(lista2)
+    arreglo_lista1 = np.array(lista1)
+    return arreglo_lista1,arreglo_lista2
+
+def matri_pro(arreglo_lista1,NumFaenas,NumDias):
+    matriz_pro = arreglo_lista1.reshape(NumFaenas,NumDias)
+    return matriz_pro
+
 def Agregar_produccion(NumFaenas,NumDias):#agregar dato en matriz lista
     for i in range(NumFaenas*NumDias):
         print(f"Ingrese Tonelada N°{i+1}: ")
@@ -27,9 +36,9 @@ def NumDia(arreglo_lista2):#dias enumeracion
     print()
     return NumDia
 
-def PlanProd(matriz2):#matriz plan de produccion
+def PlanProd(matriz_pro):#matriz plan de produccion
     n = 1
-    for fila in matriz2:
+    for fila in matriz_pro:
         print(f"Faena N°{n} ",end="  ")
         n += 1
         for elemento in fila:
@@ -60,9 +69,8 @@ while op != 5:
             NumDias = int(input("Ingrese numero de dias de produccion: "))
             Matriz_plan(NumFaenas,NumDias)
             Enumeracion_dias()
-            arreglo_lista2 = np.array(lista2)
-            arreglo_lista1 = np.array(lista1)
-            matriz_pro = arreglo_lista1.reshape(NumFaenas,NumDias)
+            arreglo_listas(lista1,lista2)
+            matri_pro(NumFaenas,NumDias)
             NumDia(arreglo_lista2)
             PlanProd(matriz_pro)
             print("---PLAN DE PRODUCCION GENERADO CON EXITO---")
@@ -81,15 +89,15 @@ while op != 5:
             arreglo_lista1 = np.array(lista1)
             matriz_pro = arreglo_lista1.reshape(NumFaenas,NumDias)
 
-
             print("---Calcular produccion total de una faena especifica---")
             num_faena = int(input("Ingrese numero de faena a calcular: "))
             prod_faena = np.sum(matriz_pro[(num_faena-1),:])
             print(prod_faena)
             
         elif op == 4:
-
-
+            arreglo_lista2 = np.array(lista2)
+            arreglo_lista1 = np.array(lista1)
+            matriz_pro = arreglo_lista1.reshape(NumFaenas,NumDias) 
 
             print("---Calcular produccion total de todas las faenas en un dia especifico---")
             num_dia = int(input("Ingrese numero del dia a calcular: "))
