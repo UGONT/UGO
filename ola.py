463bda1296bc7adb130cedbe35f3fae5f09f40b6
UGONT <134973209+UGONT@users.noreply.github.com>
2023-06-27 02:36:03 -0400

    Update ola.py

--- ola.py
+++ ola.py
@@ -1,38 +1,88 @@
 import numpy as np
 
-datos = []
-base_datos = np.array(datos)
-
-
-
-
 lista = []
 
-rut = input("Ingresa tu rut: ")
-nombre = input("Ingresa tu nombre: ")
-edad = int(input("Ingresa tu edad: "))
+def agregar_datos(lista,nif,nombre,edad,europea):
+    lista.append(nif)
+    lista.append(nombre)
+    lista.append(edad)
+    lista.append(europea)
 
-lista.append(rut)
-lista.append(nombre)
-lista.append(edad)
-arreglo_lista = np.array(lista)
+def buscar_nif(busqueda,matriz):
+    for fila in matriz:
+        if fila[0] == busqueda:
+            return True
+    return False
 
-base_datos = np.concatenate((base_datos,arreglo_lista))
+def pertenece(eu):
+    if eu == 1:
+        return True
+    else:
+        return False
+    
+def certificado(busqueda,matriz):
+    for fila in matriz:
+        if fila[0] == busqueda:
+            print(fila)
+    return False
+    
 
-lista = []
+op = 0
+while op != 4:
+    print("1.Grabar")
+    print("2.Mostrar matriz")
+    print("3.Buscar")
+    print("4.Salir")
+    op = int(input("opcion: "))
+    if op == 1:
+        nif = input("nif: ")
+        nombre = input("nombre: ")
+        edad = input("edad: ")
+        eu = int(input("Perteneces a la union europea?\n1.Si\n2.No\n->"))
+        
+        if pertenece(eu) == True:
+            europea = "Pertenece a EU"
+        else:
+            europea = "No pertenece a EU"    
+        agregar_datos(lista,nif,nombre,edad,europea)
+        arreglo_1 = np.array(lista)
+        n_filas = int(len(arreglo_1)/4)
+        matriz = arreglo_1.reshape(n_filas,4)
 
-rut = input("Ingresa tu rut: ")
-nombre = input("Ingresa tu nombre: ")
-edad = int(input("Ingresa tu edad: "))
-
-lista.append(rut)
-lista.append(nombre)
-lista.append(edad)
-arreglo_lista = np.array(lista)
-
-base_datos = np.concatenate((base_datos,arreglo_lista))
-n = len(base_datos)
-matriz = base_datos.reshape(int(n/3),3)
 
 
-print(matriz)
+    elif op == 2:
+        print("Mostrar matriz")
+        busqueda = input("Ingrese nif para imprimir su certificado: ")
+        certificado(busqueda,matriz)
+        
+        
+
+    elif op == 3:
+        print("BUSCAR")
+        if lista != []:
+            busqueda = input("Ingrese nif a buscar: ")
+            buscar_nif(busqueda,matriz)
+            if buscar_nif(busqueda,matriz) == True:
+                print("Nif encontrado")
+                print()
+            else:
+                print("Nif NO ENCONTRADO")
+        else:
+            print("No se ha agregado ningun dato")
+
+
+
+
+
+
+
+    elif op == 4:
+        print("ADIOS")
+    
+    
+
+
+
+
+
